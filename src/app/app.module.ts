03d859b57c64604636b0c1e131a15d8a02a015a5
oussama9411 <oussama.hermessi.professional@gmail.com>
2017-11-24 22:31:02 +0100

    the integration of the home page + adding the other pages.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { AgentsComponent } from './agents/agents.component';
+import { AgentsDComponent } from './agents-d/agents-d.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServiceComponent } from './service/service.component';
+import { SignComponent } from './sign/sign.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    AgentsComponent,
+    AgentsDComponent,
+    ContactComponent,
+    ServiceComponent,
+    SignComponent,
+    TestimonialsComponent
   ],
   imports: [
     BrowserModule
